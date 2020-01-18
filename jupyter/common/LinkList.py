90fa1878303b25adbb39658e74a836e31e8854f9
ubuntu-home <sysupanxl6@gmail.com>
2020-01-18 13:01:06 +0800

    【feature】将公共代码剥离到commom目录

---
+++ jupyter/common/LinkList.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+# coding=utf-8
+
+# Definition for singly-linked list.
+class ListNode:
+    
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class LinkList:
+    
+    # 根据列表创建单链表
+    def get_list(self, data):
+        if len(data) == 0:
+            return None
+        
+        temp = head = ListNode(data[0])
+        for i in range(1, len(data)):
+            temp.next = ListNode(data[i])
+            temp = temp.next
+        
+        return head
+    
+    # 逐个输出链表的内容
+    def print_list(self, head):
+        if head is None:
+            return
+        
+        while head is not None:
+            print(head.val)
+            head = head.next
