caf16b0c46f6dd9ddffc2307c50c52e8673263ba
ubuntu-home <sysupanxl6@gmail.com>
2020-01-18 12:26:34 +0800

    【feature】第2.2题

--- utils/compile.sh
+++ utils/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 declare -A file_name_map
+declare -A dir_name_map
 
 # 数组和字符串
 file_name_map['1.1']="1.1 判断字符串是否有重复的字符"
@@ -13,6 +14,15 @@ file_name_map['1.7']="1.7 将矩阵中0元素所在行、列清零"
 file_name_map['1.8']="1.8 判断是否为子字符串"
 file_name_map['1.9']="1.9 有序数组循环移动，找最小值"
 
+
+# 链表
+file_name_map['2.2']="2.2 取倒数第K的值"
+
+
+dir_name_map['1']="8.1 数组和字符串"
+dir_name_map['2']="8.2 链表"
+
 for key in "${!file_name_map[@]}"; do
-	jupyter nbconvert "jupyter/$key.ipynb" --to markdown --output "../8.1 数组和字符串/${file_name_map[$key]}.md"
+	dir_key=`echo "$key" | cut -b 1`
+	jupyter nbconvert "jupyter/$key.ipynb" --to markdown --output "../${dir_name_map[$dir_key]}/${file_name_map[$key]}.md"
 done
